4b0bdcd1f1a11384c629abe7f3a04d97de91211c
farroyo-ucr <farroyom@gmail.com>
2020-06-16 20:37:20 -0600

    Ejemplos de sincronización

---
+++ Synchronization/PThreads/Lock.cc
@@ -0,0 +1,59 @@
+/*  Esta clase encapsula las funciones para la utilizacion de semaforos
+ *  Se provee las estructuras para realizar la sincronizacion de trabajadores
+ *  a traves de los metodos tradicionales Signal (V), Wait (P)
+ *
+ *  Autor: Programacion Paralela y Concurrente
+ *  Fecha: 2020/Abr/23
+ */
+
+#include "Lock.h"
+
+
+/*
+ *
+ */
+Lock::Lock() {
+
+   this->lock = new Mutex():
+
+}
+
+
+/*
+ *
+ */
+Lock::~Lock() {
+
+   delete this->lock;
+
+}
+
+
+/*
+ *
+ */
+int Lock::Acquire() {
+
+   return this->lock->Lock();
+
+}
+
+
+/*
+ *
+ */
+int Lock::Release() {
+
+   return this->lock->Unlock();
+
+}
+
+/*
+ *
+ */
+pthread_mutex_t * Lock::getLock() {
+
+   return this->lock;
+
+}
+
